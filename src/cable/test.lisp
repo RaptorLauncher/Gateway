1fc3863fa9694921e2a37be75a4757822afe7292
Michał "phoe" Herda <phoe@disroot.org>
2018-09-24 19:27:44 +0200

    a little bit more vocal test suite for cable

--- src/cable/test.lisp
+++ src/cable/test.lisp
@@ -18,6 +18,9 @@
   (test-buffered-input)
   t)
 
+(defmacro is (form)
+  `(progn (assert ,form) (princ ".")))
+
 (defvar *input-sexp*
   '(1 2.0d0 "foo BAR \\ \" baz" foo :foo #:foo (1 2 3) (4 5)))
 
@@ -25,9 +28,9 @@
   "(1 2.0 \"foo BAR \\\\ \\\" baz\" FOO FOO FOO (1 2 3) (4 5))")
 
 (defun test-equal ()
-  (assert (cable-equal '#:foo :foo))
+  (is (cable-equal '#:foo :foo))
   (let ((data '(1 (2 3) (4.0 5) "foo" "FOO" #:foo :foo foo)))
-    (assert (cable-equal data data))))
+    (is (cable-equal data data))))
 
 (defun test-buffer ()
   (let ((gateway.cable::*stream-buffers*
@@ -37,15 +40,15 @@
           do (setf (gateway.cable::buffer-of stream) "12345")
              (close stream))
     (clean-buffers)
-    (assert (= 0 (hash-table-count gateway.cable::*stream-buffers*)))))
+    (is (= 0 (hash-table-count gateway.cable::*stream-buffers*)))))
 
 (defun test-output ()
   (let* ((result (with-output-to-string (s) (to-cable *input-sexp* s))))
-    (assert (string= result *input-string*))))
+    (is (string= result *input-string*))))
 
 (defun test-input ()
   (let* ((result (with-input-from-string (s *input-string*) (from-cable s))))
-    (assert (cable-equal result *input-sexp*))))
+    (is (cable-equal result *input-sexp*))))
 
 (defun test-buffered-input ()
   (%test-buffered-input "(1 2 3 " "4 5 6)" '(1 2 3 4 5 6))
@@ -55,7 +58,7 @@
   (let ((pipe (cl-plumbing:make-pipe)))
     (loop for char across left
           do (write-char char pipe))
-    (assert (null (from-cable-buffered pipe)))
+    (is (null (from-cable-buffered pipe)))
     (loop for char across right
           do (write-char char pipe))
-    (assert (equal expected (from-cable-buffered pipe)))))
+    (is (equal expected (from-cable-buffered pipe)))))
