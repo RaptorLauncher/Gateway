df27f44a29cad0feac5604e97611802f52ba0826
Michał "phoe" Herda <phoe@disroot.org>
2018-10-01 09:08:23 +0200

    assertion descriptions

--- src/engine/impl/data-message.lisp
+++ src/engine/impl/data-message.lisp
@@ -19,21 +19,27 @@
       (message-read-error 'message-read-error data e))))
 
 (defun check-id-valid (id)
+  ;; TODO assertion descriptions for DATA-OBJECT as well
   (handler-case
       (destructuring-bind (owner number) id
-        (assert (member owner '(:c :s) :test #'cable-equal))
-        (assert (typep number 'unsigned-byte)))
+        (assert (member owner '(:c :s) :test #'cable-equal) ()
+                "The first part of the ID should be C or S, not ~S." owner)
+        (assert (typep number 'unsigned-byte) ()
+                "The second part of the ID should be a number, not ~S." number))
     (error (e) ;; TODO logging everywhere
       (error (message-read-error 'invalid-message-id id e)))))
 
 (defun check-message-type-valid (message-type)
   (let ((type '(or symbol (cons symbol (cons symbol null)))))
     (handler-case
-        (progn (assert (typep message-type type))
+        (progn (assert (typep message-type type) ()
+                       "Message type should be a symbol or a two-element list, ~
+                        not ~S." message-type)
                (when (listp message-type)
                  (let ((status (first message-type)))
-                   (assert (or (cable-equal status :ok)
-                               (cable-equal status :error))))))
+                   (assert (member status '(:ok :error) :test #'cable-equal) ()
+                           "The message status should be OK or ERROR, not ~S."
+                           status))))
       (error (e)
         (message-read-error 'invalid-message-type message-type e)))))
 
@@ -42,8 +48,11 @@
       (let* ((subclasses (subclasses (find-class 'standard-message)))
              (class (find message-type subclasses :test #'string=
                                                   :key #'class-name)))
-        (assert (not (null class)))
-        (assert (not (protocol-object-p class)))
+        (assert (not (null class)) ()
+                "Message type ~S was not found." message-type)
+        (assert (not (protocol-object-p class)) ()
+                "Message type ~S is a protocol message type and can't be ~
+                 instantiated." message-type)
         class)
     (error (e)
       (message-read-error 'invalid-message-type message-type e))))
