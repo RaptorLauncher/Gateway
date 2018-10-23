79cbd7053901832620276e31940123732c3daa7d
Michał "phoe" Herda <phoe@disroot.org>
2018-10-23 18:45:01 +0200

    extend hello messages

--- src/messages/hello.lisp
+++ src/messages/hello.lisp
@@ -1,14 +1,33 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; GATEWAY
 ;;;; © Michał "phoe" Herda 2016-2018
-;;;; connector/message/hello.lisp
+;;;; messages/hello.lisp
 
 (in-package #:gateway.engine/message)
 
 (define-protocol-class (:hello) () ())
 
-(defclass (:request :hello) ((:request) (:hello)) ())
+(defclass (:request :hello) ((:request) (:hello))
+  ((%client :accessor client
+            :initarg :client)
+   (%version :accessor version
+             :initarg :version))
+  (:default-initargs :client (error "Must provide CLIENT.")
+                     :version (error "Must provide VERSION.") ;; TODO *version*
+   ))
 
-(defclass (:response :hello) ((:response) (:hello)) ())
+(defclass (:response :hello) ((:response) (:hello))
+  ((%server :accessor server
+            :initarg :server)
+   (%version :accessor version
+             :initarg :version)
+   (%domain :accessor domain
+            :initarg :domain))
+  (:default-initargs
+   :client (error "Must provide CLIENT.")
+   :version (error "Must provide VERSION.") ;; TODO *version*
+   :domain #+sbcl (sb-bsd-sockets:host-ent-name
+                   (sb-bsd-sockets:get-host-by-name "localhost"))
+           #-sbcl (machine-instance)))
 
 (defclass (:error :hello) ((:error) (:hello)) ())
