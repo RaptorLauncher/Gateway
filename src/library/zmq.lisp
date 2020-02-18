04bceb74066410b8f20812cadd91c7a944110344
Michał "phoe" Herda <phoe@disroot.org>
2020-02-18 12:09:17 +0100

    Factor zmq

--- src/library/zmq.lisp
+++ src/library/zmq.lisp
@@ -1,3 +1,21 @@
+(ql:quickload '(:pzmq :gateway.cable))
+
+;; Utils
+
+(defmacro kill-socket (place)
+  `(when ,place
+     (pzmq:close ,place)
+     (setf ,place nil)))
+
+(defmacro kill-context (place)
+  `(when ,place
+     (pzmq:ctx-term ,place)
+     (setf ,place nil)))
+
+(defun set-socket-options (socket &rest args)
+  (loop for (key value) on args by #'cddr
+        do (pzmq:setsockopt socket key value)))
+
 ;; Client networking
 
 (defparameter *client-context* nil)
@@ -5,22 +23,15 @@
 (defparameter *client-socket* nil)
 
 (defun start-client (&optional (server-address "tcp://localhost:6500"))
-  (let ((context (pzmq:ctx-new)))
-    (setf *client-context* context)
-    (let* ((socket (pzmq:socket context :dealer)))
-      (pzmq:setsockopt socket :linger 100)
-      (pzmq:connect socket server-address)
-      (setf *client-socket* socket))))
+  (setf *client-context* (pzmq:ctx-new))
+  (setf *client-socket* (pzmq:socket *client-context* :dealer))
+  (set-socket-options *client-socket*
+                      :linger 100)
+  (pzmq:connect *client-socket* server-address))
 
 (defun stop-client ()
-  (when *client-socket*
-    (pzmq:close *client-socket*)
-    (setf *client-socket* nil))
-  (when *client-context*
-    (pzmq:ctx-term *client-context*)
-    (setf *client-context* nil)))
-
-
+  (kill-socket *client-socket*)
+  (kill-context *client-context*))
 
 ;; Client logic
 
@@ -28,11 +39,11 @@
   (format t "~&Sending ping...")
   (let ((data '(ping (1 2 :hello "foo" 3))))
     (print data)
-    (pzmq:send *client-socket* (prin1-to-string data)))
+    (pzmq:send *client-socket*
+               (with-output-to-string (stream) (gateway.cable:to-cable data stream))))
   (format t "~&Receiving...")
-  (print (read-from-string (pzmq:recv-string *client-socket*))))
-
-
+  (print (with-input-from-string (stream (pzmq:recv-string *client-socket*))
+           (gateway.cable:from-cable stream))))
 
 ;; Server networking
 
@@ -41,28 +52,19 @@
 (defparameter *server-socket* nil)
 
 (defun stop-server ()
-  (when *server-socket*
-    (pzmq:close *server-socket*)
-    (setf *server-socket* nil))
-  (when *server-context*
-    (pzmq:ctx-term *server-context*)
-    (setf *server-context* nil)))
+  (kill-socket *server-socket*)
+  (kill-context *server-context*))
 
 (defun start-server (&optional (listen-address "tcp://*:6500"))
-  (let ((complete-p nil))
-    (unwind-protect
-         (let ((context (pzmq:ctx-new)))
-           (setf *server-context* context)
-           (let* ((socket (pzmq:socket context :router)))
-             (setf *server-socket* socket)
-             (pzmq:setsockopt socket :router-handover 1)
-             (pzmq:setsockopt socket :heartbeat-ivl 10000)
-             (pzmq:setsockopt socket :connect-timeout 30000)
-             (pzmq:setsockopt socket :heartbeat-timeout 30000)
-             (pzmq:setsockopt socket :immediate 1)
-             (pzmq:bind socket listen-address)
-             (setf complete-p t)))
-      (unless complete-p (stop-server)))))
+  (setf *server-context* (pzmq:ctx-new))
+  (setf *server-socket* (pzmq:socket *server-context* :router))
+  (set-socket-options *server-socket*
+                      :immediate 1
+                      :router-handover 1
+                      :heartbeat-ivl 10000
+                      :connect-timeout 30000
+                      :heartbeat-timeout 30000)
+  (pzmq:bind *server-socket* listen-address))
 
 ;; Server logic
 
@@ -80,12 +82,15 @@
                        :count (pzmq:msg-size message))))
             (cffi:with-pointer-to-vector-data (pointer identity)
               (pzmq:send socket pointer :len 5 :sndmore t))
-            (let ((request (read-from-string data))
+            (let ((request (with-input-from-string (stream data)
+                             (gateway.cable:from-cable stream)))
                   (response nil))
               (if (eq (first request) 'ping)
                   (setf response `(pong ,(second request)))
                   (setf response `(huh? ,request)))
-              (pzmq:send socket (prin1-to-string response)))))))))
+              (pzmq:send socket
+                         (with-output-to-string (stream)
+                           (gateway.cable:to-cable response stream))))))))))
 
 (defun retransmit (from to)
   (pzmq:with-message message
