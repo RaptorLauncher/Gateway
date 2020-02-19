be6544030ea7d80c6555daf278afde8f665796bb
Michał "phoe" Herda <phoe@disroot.org>
2020-02-19 11:33:17 +0100

    Work around qtools bug #39

--- src/client/ui/main.lisp
+++ src/client/ui/main.lisp
@@ -7,6 +7,12 @@
 (in-readtable :qtools)
 
 (defun main ()
+  (dolist (lib '("smokebase" "smokeqtcore" "smokeqtgui"))
+    (cffi:load-foreign-library (format nil
+                                       #+win32 "~A.dll"
+                                       #+darwin "qtlibs!~A.dylib"
+                                       #+linux "qtlibs!~A.so"
+                                       lib)))
   (with-main-window (window (q+:make-qlabel "Hello world!"))
     (setf (q+:minimum-height window) 300
           (q+:minimum-width window) 400)))
